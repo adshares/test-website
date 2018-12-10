450eb031863b08ddc5617bc482ff3f4941ac7a01
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-12-10 19:07:24 +0100

    new server names

--- publisher/config/servicesConfig.js
+++ publisher/config/servicesConfig.js
@@ -1,7 +1,7 @@
 const SERVICES_URLS = {
     1: 'http://localhost:8101/supply/find.js',
-    2: 'http://localhost:8101/supply/find.js',
-    3: 'http://localhost:8101/supply/find.js',
+    2: 'http://server.dock:8101/supply/find.js',
+    3: 'http://server.ads:8101/supply/find.js',
 };
 
 export default SERVICES_URLS;
