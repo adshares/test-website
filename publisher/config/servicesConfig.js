69258c8be2293128192826d3d5991ef7511dbd28
Paweł Zaremba <pawel.zaremba@adshares.pl>
2018-12-31 17:58:42 +0100

    rendom file

--- publisher/config/servicesConfig.js
+++ publisher/config/servicesConfig.js
@@ -1,5 +1,5 @@
 const SERVICES_URLS = {
-    1: 'https://demo-server.adshares.net/supply/find.js',
+    1: 'https://demo-server.adshares.net/supply/find.js?n='+Math.random(),
 };
 
 export default SERVICES_URLS;
