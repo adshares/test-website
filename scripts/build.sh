cbc0fec21d5c330fbf169812561bbec1ab280965
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-01-30 12:04:30 +0100

    dist zones

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,13 +19,7 @@ if [ ! -v TRAVIS ]; then
   cd ${BUILD_PATH}/build
 fi
 
-export SITE_ZONE_01=${SITE_ZONE_01:-"950A658CACB246F8A142EFB722A2ACD8"}
-export SITE_ZONE_02=${SITE_ZONE_02:-"950A658CACB246F8A142EFB722A2ACD9"}
-export SITE_ZONE_03=${SITE_ZONE_03:-"950A658CACB246F8A142EFB722A2ACDA"}
-export SITE_ZONE_04=${SITE_ZONE_04:-"950A658CACB246F8A142EFB722A2ACDB"}
-
 envsubst < servicesConfig.js.dist | tee publisher/config/servicesConfig.js
-envsubst < index.js.dist | tee publisher/demo/src/Data/index.js
 
 cd publisher/demo
 
--- servicesConfig-demo.js
+++
@@ -1,5 +0,0 @@
-const SERVICES_URLS = {
-    1: 'https://demo-server.adshares.net/supply/find.js',
-};
-
-export default SERVICES_URLS;
