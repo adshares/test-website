5d01baf823c3bfd8b20f3964ed966faf169aa687
Meskat <marciniszyn.k@gmail.com>
2019-01-31 14:06:29 +0100

    Open social media links in the new card

--- publisher/demo/src/components/Footer/Footer.js
+++ publisher/demo/src/components/Footer/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({children}) => (
         <BottomInfo>
             <Address>
                 <CompanyName>Publisher site</CompanyName>
-                    <Link src="https://adshares.net/" >
+                    <Link src="https://adshares.net/" target="_blanc" >
                         https://adshares.net/
                     </Link>
                 <Email href="mailto:office@adshares.net"> office@adshares.net </Email>
@@ -31,21 +31,21 @@ const Footer = ({children}) => (
         </BottomInfo>
         <BottomInfo>
             <SocialMedia>
-                <ImageLink href="https://medium.com/adshares/">
+                <ImageLink href="https://medium.com/adshares/" target="_blanc">
                     <Icon src={medium} alt="medium"/>
                 </ImageLink>
 
-                <ImageLink href="https://t.me/adshares">
+                <ImageLink href="https://t.me/adshares" target="_blanc">
                     <Icon src={telegram} alt="telegram"/>
                 </ImageLink>
-                <ImageLink href="https://www.reddit.com/r/adshares/">
+                <ImageLink href="https://www.reddit.com/r/adshares/" target="_blanc">
                     <Icon src={reddit} alt="reddit"/>
                 </ImageLink>
-                <ImageLink href="https://twitter.com/adsharesNet">
+                <ImageLink href="https://twitter.com/adsharesNet" target="_blanc">
                     <Icon src={twitter} alt="twitter"/>
                 </ImageLink>
 
-                <ImageLink href="https://www.linkedin.com/company/adshares/">
+                <ImageLink href="https://www.linkedin.com/company/adshares/ target"_blanc>
                     <Icon src={linkedIn} alt="twitter"/>
                 </ImageLink>
             </SocialMedia>
